644aa2dd0db8986ec243c2d9e519a9072c5b3212
Gastón Sobrevilla <gastonsobrevilla@gmail.com>
2020-04-15 10:37:04 -0300

    login ui

--- Curso/ViewController.swift
+++ Curso/ViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet private var label: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        let storyboard = UIStoryboard(name: "LoginViewController", bundle: .main)
+        let loginVC = storyboard.instantiateViewController(identifier: "LoginViewController")
+        
+        navigationController?.pushViewController(loginVC, animated: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+    }
+    
+    @IBAction private func buttonTapped() {
+        label?.textColor = UIColor.red
+        print("algo")
     }
 }
 
---
+++ Curso/ViewControllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Curso
+//
+//  Created by Gastón Sobrevilla on 15/04/2020.
+//  Copyright © 2020 Vortex Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet private var usernameTextField: UITextField?
+    @IBOutlet private var passwordTextField: UITextField?
+    @IBOutlet private var button: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button?.layer.cornerRadius = 8
+    }
+    
+    @IBAction private func buttonTapped() {
+        if let username = usernameTextField?.text, let password = passwordTextField?.text {
+            print(username)
+            print(password)
+        }
+    }
+}
